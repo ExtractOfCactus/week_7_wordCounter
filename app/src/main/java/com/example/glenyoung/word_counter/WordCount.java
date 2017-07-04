045709debefd66718c4a7d62092af0e369060afe
Glen Young <extractofcactus@icloud.com>
2017-07-04 18:18:29 +0100

    working word counter app

--- app/src/main/java/com/example/glenyoung/word_counter/WordCount.java
+++ app/src/main/java/com/example/glenyoung/word_counter/WordCount.java
@@ -2,12 +2,37 @@ package com.example.glenyoung.word_counter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class WordCount extends AppCompatActivity {
+    EditText sentenceText;
+    TextView answerText;
+    Button countWords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_count);
+
+        Log.d(getClass().toString(), "onCreate called");
+
+        sentenceText = (EditText) findViewById(R.id.string_text);
+        answerText = (TextView) findViewById(R.id.number_of_words);
+        countWords = (Button) findViewById(R.id.count_button);
+    }
+
+    public void onCountWordsButtonClicked(View button) {
+        String sentence = sentenceText.getText().toString();
+        Log.d(getClass().toString(), "onCountWordsButtonClicked was called");
+        Log.d(getClass().toString(), "Sentence given: " + sentence);
+        Counter counter = new Counter();
+        int wordCount = counter.numberOfWords(sentence);
+        answerText.setText("" + wordCount);
     }
 }
